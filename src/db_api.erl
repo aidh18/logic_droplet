4c6316823531ece93f1b2feec999431c91d90f53
Aidan <jaidanstacey@gmail.com>
2024-07-15 17:23:35 -0700

    Added more apis for names, settings, hours, and adding employees

--- src/db_api.erl
+++ src/db_api.erl
@@ -2,45 +2,134 @@
 -export([store_data/4,retrieve_data/3]).
 
 
-store_data(Table_name,Key,Value,Pid) when not is_tuple(Value)->
-	Table_bin = list_to_binary(Table_name),
-	Key_bin = list_to_binary(Key),
-	Value_bin = list_to_binary(Value),
-	case retrieve_object(Table_bin, Key_bin, Pid) of
-		{ok,Object}->
-			New_object = riakc_obj:update_value(Object, Value_bin),
-            {reply,riakc_pb_socket:put(Pid, New_object),Pid};
-		_->
-			New_object = riakc_obj:new(Table_bin, Key_bin, Value_bin),
-			{reply,riakc_pb_socket:put(Pid, New_object),Pid}
-	end;
-store_data(Table_name,Key,{Date,Hours},Pid)->
-	Table_bin = list_to_binary(Table_name),
-	Key_bin = list_to_binary(Key),
-	case retrieve_object(Table_bin, Key_bin, Pid) of
+store_data(settings,User_id,{Setting_type,Setting},Pid)->
+	Table_bin = list_to_binary("Settings"),
+	Key_bin = list_to_binary(User_id),
+	case retrieve_object(Table_bin,Key_bin,Pid) of
 		{ok,Object}->
 			Map = binary_to_term(riakc_obj:get_value(Object)),
-			case maps:find(Date, Map) of
-				{ok, Value} ->
-					New_map = Map#{Date := Value ++ Hours},
+			case maps:find(Setting,Map) of
+				{ok,_Current} ->
+					New_map = Map#{Setting_type := Setting},
 					Map_bin = term_to_binary(New_map),
-					New_object = riakc_obj:update_value(Object, Map_bin),
-            		{reply,riakc_pb_socket:put(Pid, New_object),Pid};
+					New_object = riakc_obj:update_value(Object,Map_bin),
+            		{reply,riakc_pb_socket:put(Pid,New_object),Pid};
+				error ->
+					New_map = Map#{Setting_type=> Setting},
+					Map_bin = term_to_binary(New_map),
+					New_object = riakc_obj:update_value(Object,Map_bin),
+            		{reply,riakc_pb_socket:put(Pid,New_object),Pid}
+			end;
+		_->
+			Map_bin = term_to_binary(#{Setting_type=> Setting}),
+			New_object = riakc_obj:new(Table_bin,Key_bin,Map_bin),
+			{reply,riakc_pb_socket:put(Pid,New_object),Pid}
+	end;
+store_data(Table_name,num,Employee_id,Pid)->
+		Table_bin = list_to_binary(Table_name),
+		Num_bin = list_to_binary("Num_of_employees"),
+		Employee_id_bin = term_to_binary(Employee_id),
+		case retrieve_object(Table_bin,Num_bin,Pid) of
+			{ok,Object}->
+				Num_of_employees = binary_to_integer(riakc_obj:get_value(Object)),
+				New_number_bin = integer_to_binary(Num_of_employees + 1),
+				Updated_number = riakc_obj:update_value(Object,Num_of_employees + 1),
+				riakc_pb_socket:put(Pid,Updated_number),
+				New_object = riakc_obj:new(Table_bin,New_number_bin,Employee_id_bin),
+				{reply,riakc_pb_socket:put(Pid,New_object),Pid};
+			_->
+				New_number_bin = integer_to_binary(0),
+				New_num_object = riakc_obj:new(Table_bin,Num_bin,New_number_bin),
+				riakc_pb_socket:put(Pid,New_num_object),
+				New_object = riakc_obj:new(Table_bin,New_number_bin,Employee_id_bin),
+				{reply,riakc_pb_socket:put(Pid,New_object),Pid}
+		end;
+store_data(Table_name,Key,{login,Password,User_id},Pid)->
+	Table_bin = list_to_binary(Table_name),
+	Key_bin = list_to_binary(Key),
+	User_bin = term_to_binary(User_id),
+	case retrieve_object(Table_bin,Key_bin,Pid) of
+		{ok,Object}->
+			New_object = riakc_obj:update_value(Object,{Password,User_bin}),
+            {reply,riakc_pb_socket:put(Pid,New_object),Pid};
+		_->
+			New_object = riakc_obj:new(Table_bin,Key_bin,{Password,User_bin}),
+			{reply,riakc_pb_socket:put(Pid,New_object),Pid}
+	end;
+store_data(Table_name,Key,{hours,Date,Hours},Pid)->
+	Table_bin = list_to_binary(Table_name),
+	Key_bin = list_to_binary(Key),
+	case retrieve_object(Table_bin,Key_bin,Pid) of
+		{ok,Object}->
+			Map = binary_to_term(riakc_obj:get_value(Object)),
+			case maps:find(Date,Map) of
+				{ok,Value} ->
+					New_map = Map#{Date := Value + Hours},
+					Map_bin = term_to_binary(New_map),
+					New_object = riakc_obj:update_value(Object,Map_bin),
+            		{reply,riakc_pb_socket:put(Pid,New_object),Pid};
 				error ->
 					New_map = Map#{Date=> Hours},
 					Map_bin = term_to_binary(New_map),
-					New_object = riakc_obj:update_value(Object, Map_bin),
-            		{reply,riakc_pb_socket:put(Pid, New_object),Pid}
+					New_object = riakc_obj:update_value(Object,Map_bin),
+            		{reply,riakc_pb_socket:put(Pid,New_object),Pid}
 			end;
 		_->
 			Map_bin = term_to_binary(#{Date=> Hours}),
-			New_object = riakc_obj:new(Table_bin, Key_bin, Map_bin),
-			{reply,riakc_pb_socket:put(Pid, New_object),Pid}
+			New_object = riakc_obj:new(Table_bin,Key_bin,Map_bin),
+			{reply,riakc_pb_socket:put(Pid,New_object),Pid}
+	end;
+store_data(Table_name,Key,Value,Pid)->
+	Table_bin = list_to_binary(Table_name),
+	Key_bin = list_to_binary(Key),
+	Value_bin = list_to_binary(Value),
+	case retrieve_object(Table_bin,Key_bin,Pid) of
+		{ok,Object}->
+			New_object = riakc_obj:update_value(Object,Value_bin),
+            {reply,riakc_pb_socket:put(Pid,New_object),Pid};
+		_->
+			New_object = riakc_obj:new(Table_bin,Key_bin,Value_bin),
+			{reply,riakc_pb_socket:put(Pid,New_object),Pid}
 	end.
 
 
+retrieve_data(settings,Key,Pid)->
+	case retrieve_object(list_to_binary("Settings"),list_to_binary(Key),Pid) of
+		{ok,Object}->
+			{reply,binary_to_term(riakc_obj:get_value(Object)),Pid};
+		Error->
+			{reply,Error,Pid}
+	end;
+retrieve_data(Table_name,{index,Key},Pid)->
+	case retrieve_object(list_to_binary(Table_name),integer_to_binary(Key),Pid) of
+		{ok,Object}->
+			{reply,binary_to_term(riakc_obj:get_value(Object)),Pid};
+		Error->
+			{reply,Error,Pid}
+	end;
+retrieve_data(employees,Key,Pid)->
+	case retrieve_object(list_to_binary("Employees"),list_to_binary(Key),Pid) of
+		{ok,Object}->
+			{reply,binary_to_term(riakc_obj:get_value(Object)),Pid};
+		Error->
+			{reply,Error,Pid}
+	end;
+retrieve_data(Table_name,first,Pid)->
+    case retrieve_object(list_to_binary(Table_name),list_to_binary("Num_of_employees"),Pid) of
+	    {ok,Object}->
+			{reply,binary_to_term(riakc_obj:get_value(Object)),Pid};
+	    Error->
+			{reply,Error,Pid}
+	end;
 retrieve_data(Table_name,Key,Pid) when is_list(Table_name)->
     case retrieve_object(list_to_binary(Table_name),list_to_binary(Key),Pid) of
+	    {ok,Object}->
+			{reply,binary_to_list(riakc_obj:get_value(Object)),Pid};
+	    Error->
+			{reply,Error,Pid}
+	end;
+retrieve_data(usernames,Key,Pid)->
+    case retrieve_object(list_to_binary("Usernames"),list_to_binary(Key),Pid) of
 	    {ok,Object}->
 			{reply,binary_to_term(riakc_obj:get_value(Object)),Pid};
 	    Error->
@@ -49,11 +138,11 @@ retrieve_data(Table_name,Key,Pid) when is_list(Table_name)->
 retrieve_data(Table_name,Key,Pid)->
     case retrieve_object(list_to_binary(Table_name),list_to_binary(Key),Pid) of
 	    {ok,Object}->
-			{reply,binary_to_list(riakc_obj:get_value(Object)),Pid};
+			{reply,binary_to_term(riakc_obj:get_value(Object)),Pid};
 	    Error->
 			{reply,Error,Pid}
 	end.
 
 
 retrieve_object(Table_name,Key,Pid)->
-	riakc_pb_socket:get(Pid, Table_name, Key).
+	riakc_pb_socket:get(Pid,Table_name,Key).
--- src/logic.erl
+++ src/logic.erl
@@ -14,10 +14,10 @@
 -export([start/0,start/3,stop/0]).
 
 %% gen_server callbacks
--export([init/1,request_hours_api/1,request_login_api/3,update_hours_api/3,
-         update_login_api/2,get_hours/2,get_employees/3,handle_call/3,
-         handle_cast/2,handle_info/2,terminate/2,code_change/3]).
-
+-export([init/1,request_hours_api/2,request_login_api/2,request_name_api/1,
+request_settings_api/1,update_hours_api/3,update_login_api/3,update_name_api/2,
+update_settings/3,get_hours/2,get_employees/4,add_employees_api/2,test/2,
+handle_call/3,handle_cast/2,handle_info/2,terminate/2,code_change/3]).
 
 %%%===================================================================
 %%% API
@@ -60,17 +60,20 @@ start(Registration_type,Name,Args)->
 stop()-> gen_server:call(?MODULE,stop).
 
 %% Any other API functions go here.
-
-request_hours_api(User_id)->
+add_employees_api(Employer_id,Employee_id)->
     Node = rrobin:next(),
-    case gen_server:call(Node,{request_hours,User_id}) of
+    gen_server:cast(Node,{add_employees,{Employer_id,Employee_id}}).
+
+request_hours_api(Is_employer,User_id)->
+    Node = rrobin:next(),
+    case gen_server:call(Node,{request_hours,{Is_employer,User_id}}) of
         {error,invalid_id}-> 500;
         {error,empty_id}-> 500;
         {error,notfound}-> 500;
         Response-> Response
     end.
 
-request_login_api(_User_id,Username,Password)->
+request_login_api(Username,Password)->
     Node = rrobin:next(),
     case gen_server:call(Node,{request_login,{Username,Password}}) of
         {error,invalid_id}-> 500;
@@ -79,24 +82,55 @@ request_login_api(_User_id,Username,Password)->
         Response-> Response
     end.
 
+request_name_api(User_id)->
+    Node = rrobin:next(),
+    case gen_server:call(Node,{request_name,User_id}) of
+        {error,invalid_id}-> 500;
+        {error,empty_id}-> 500;
+        {error,notfound}-> 500;
+        Response-> Response
+    end.
+
+request_settings_api(User_id)->
+    Node = rrobin:next(),
+    case gen_server:call(Node,{request_settings,User_id}) of
+        {error,invalid_id}-> 500;
+        {error,empty_id}-> 500;
+        {error,notfound}-> 500;
+        Response-> Response
+    end.
+
+update_settings(User_id,Setting_type,Setting)->
+    Node = rrobin:next(),
+    gen_server:cast(Node,{update_settings,{User_id,Setting_type,Setting}}).
+
 update_hours_api(User_id,Date,Hours)->
     Node = rrobin:next(),
     gen_server:cast(Node,{update_hours,{User_id,Date,Hours}}).
 
-update_login_api(Username,New_password)->
+update_login_api(User_id,Username,New_password)->
     Node = rrobin:next(),
-    gen_server:cast(Node,{update_login,{Username,New_password}}).
+    gen_server:cast(Node,{update_login,{User_id,Username,New_password}}).
 
+update_name_api(User_id,Name)->
+    Node = rrobin:next(),
+    gen_server:cast(Node,{update_name,{User_id,Name}}).
 
 get_hours(Employee_id,Db_pid)->
-    db_api:retrieve_data("Employees",Employee_id,Db_pid).
+    Name = request_name_api(Employee_id),
+    {_,Hours,_} = db_api:retrieve_data(employees,Employee_id,Db_pid),
+    [Name,Hours].
 
-get_employees(Employees,Hours,_) when Employees =:= []->
+get_employees(_,Index,Hours,_) when Index < 0->
     Hours;
-get_employees(Employees,Hours,Db_pid)->
-    [H|T] = Employees,
-    Data = get_hours(H,Db_pid),
-    get_employees(T,Hours ++ Data,Db_pid).
+get_employees(Employer_id,Index,Hours,Db_pid)->
+    {_,Employee_id,_} = db_api:retrieve_data(Employer_id,{index,Index},Db_pid),
+    Data = get_hours(Employee_id,Db_pid),
+    get_employees(Employer_id,Index - 1,Hours ++ [Data],Db_pid).
+
+test(Table_name,Key)->
+    Node = rrobin:next(),
+    gen_server:call(Node,{test,{Table_name,Key}}).
 
 
 %%%===================================================================
@@ -116,7 +150,7 @@ init([])->
         {ok,Db_pid}-> {ok,Db_pid};
         _-> {stop,link_failure}
     end.
-
+% logic:request_hours_api(true,"testboi1").
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
@@ -131,7 +165,7 @@ init([])->
                                   {noreply,term(),integer()}  |
                                   {stop,term(),term(),integer()} |
                                   {stop,term(),term()}.
-handle_call({request_hours,User_id},_From,Db_pid)->
+handle_call({request_hours,{Is_employer,User_id}},_From,Db_pid)->
     if
         not is_list(User_id)->
             {reply,{error,invalid_id},Db_pid};
@@ -140,15 +174,14 @@ handle_call({request_hours,User_id},_From,Db_pid)->
                 true->
                     {reply,{error,empty_id},Db_pid};
                 _->
-                    [H|_] = User_id,
                     if
-                        H =:= "W"->
-                            {_,Employees,_} = db_api:retrieve_data("Employers",
-                                                                User_id,
+                        Is_employer->
+                            {_,Num_of_employees,_} = db_api:retrieve_data(User_id,
+                                                                first,
                                                                 Db_pid),
                             if
-                                is_list(Employees)->
-                                    get_employees(Employees,[],Db_pid);
+                                is_integer(Num_of_employees)->
+                                    {reply,get_employees(User_id,Num_of_employees,[],Db_pid),Db_pid};
                                 true->
                                     {reply,{error,invalid_id,Db_pid}}
                             end;
@@ -164,21 +197,50 @@ handle_call({request_login,{Username,Password}},_From,Db_pid)->
         not is_list(Password)->
             {reply,{error,invalid_password},Db_pid};
         true->
-            case Username =:= "" of
-                true->
+            if
+                Username =:= ""->
                     {reply,{error,empty_username},Db_pid};
-                _->
-                    {_,{Saved,User_id},_} = db_api:retrieve_data("Usernames",
+                Password =:= ""->
+                    {reply,{error,empty_username},Db_pid};
+                true->
+                    {_,{Saved,User_id},_} = db_api:retrieve_data(usernames,
                                                                 Username,
                                                                 Db_pid),
+                    Hashed = crypto:hash(sha256,Password),
                     if
-                        Saved =:= Password->
-                            {reply,User_id,Db_pid};
+                        Saved == Hashed->
+                            {reply,binary_to_term(User_id),Db_pid};
                         true->
                             {reply,invalid_password,Db_pid}
                     end
             end
     end;
+handle_call({request_name,User_id},_From,Db_pid)->
+    if
+        not is_list(User_id)->
+            {reply,{error,invalid_id},Db_pid};
+        true->
+            if
+                User_id =:= ""->
+                    {reply,{error,empty_id},Db_pid};
+                true->
+                    db_api:retrieve_data("Names",User_id,Db_pid)
+            end
+    end;
+handle_call({request_settings,User_id},_From,Db_pid)->
+    if
+        not is_list(User_id)->
+            {reply,{error,invalid_id},Db_pid};
+        true->
+            if
+                User_id =:= ""->
+                    {reply,{error,empty_id},Db_pid};
+                true->
+                    db_api:retrieve_data(settings,User_id,Db_pid)
+            end
+    end;
+handle_call({test,{Table_name,Key}},_From,Db_pid)->
+    db_api:retrieve_data(Table_name,Key,Db_pid);
 handle_call(stop,_From,_State)->
         {stop,normal,
                 replace_stopped,
@@ -206,13 +268,16 @@ handle_cast({update_hours,{User_id,Date,Hours}},Db_pid)->
                     io:format("update_hours: User_id is empty."),
                     {noreply,Db_pid};
                 _->
-                    db_api:store_data("Employees",User_id,{Date,Hours},
+                    db_api:store_data("Employees",User_id,{hours,Date,Hours},
                                                 Db_pid),
                     {noreply,Db_pid}
             end
     end;
-handle_cast({update_login,{Username,New_password}},Db_pid)->
+handle_cast({update_login,{User_id,Username,New_password}},Db_pid)->
     if
+        not is_list(User_id)->
+            io:format("update_login: User_id is not a list."),
+            {noreply,Db_pid};
         not is_list(Username)->
             io:format("update_login: Username is not a list."),
             {noreply,Db_pid};
@@ -221,6 +286,9 @@ handle_cast({update_login,{Username,New_password}},Db_pid)->
             {noreply,Db_pid};
         true->
             if
+                User_id =:= ""->
+                    io:format("update_login: User_id is empty."),
+                    {noreply,Db_pid};
                 Username =:= ""->
                     io:format("update_login: Username is empty."),
                     {noreply,Db_pid};
@@ -228,7 +296,81 @@ handle_cast({update_login,{Username,New_password}},Db_pid)->
                     io:format("update_login: New_password is empty."),
                     {noreply,Db_pid};
                 true->
-                    db_api:store_data("Locations",Username,New_password,Db_pid),
+                    db_api:store_data("Usernames",Username,
+                                    {login,crypto:hash(sha256,New_password),
+                                    User_id},Db_pid),
+                    {noreply,Db_pid}
+            end
+    end;
+handle_cast({update_name,{User_id,Name}},Db_pid)->
+    if
+        not is_list(User_id)->
+            io:format("update_name: User_id is not a list."),
+            {noreply,Db_pid};
+        not is_list(Name)->
+            io:format("update_name: Name is not a list."),
+            {noreply,Db_pid};
+        true->
+            if
+                User_id =:= ""->
+                    io:format("update_name: User_id is empty."),
+                    {noreply,Db_pid};
+                Name =:= ""->
+                    io:format("update_name: Name is empty."),
+                    {noreply,Db_pid};
+                true->
+                    db_api:store_data("Names",User_id,Name,
+                                                Db_pid),
+                    {noreply,Db_pid}
+            end
+    end;
+handle_cast({update_settings,{User_id,Setting_type,Setting}},Db_pid)->
+    if
+        not is_list(User_id)->
+            io:format("update_settings: User_id is not a list."),
+            {noreply,Db_pid};
+        not is_list(Setting_type)->
+            io:format("update_settings: Setting_type is not a list."),
+            {noreply,Db_pid};
+        not is_list(Setting)->
+            io:format("update_settings: Setting is not a list."),
+            {noreply,Db_pid};
+        true->
+            if
+                User_id =:= ""->
+                    io:format("update_settings: User_id is empty."),
+                    {noreply,Db_pid};
+                Setting_type =:= ""->
+                    io:format("update_settings: Setting_type is empty."),
+                    {noreply,Db_pid};
+                Setting =:= ""->
+                    io:format("update_settings: Setting is empty."),
+                    {noreply,Db_pid};
+                true->
+                    db_api:store_data(settings,User_id,{Setting_type,Setting},
+                                                Db_pid),
+                    {noreply,Db_pid}
+            end
+    end;
+handle_cast({add_employees,{Employer_id,Employee_id}},Db_pid)->
+    if
+        not is_list(Employer_id)->
+            io:format("update_login: Employer_id is not a list."),
+            {noreply,Db_pid};
+        not is_list(Employee_id)->
+            io:format("update_login: Employee_id is not a list."),
+            {noreply,Db_pid};
+        true->
+            if
+                Employer_id =:= ""->
+                    io:format("update_login: Employer_id is empty."),
+                    {noreply,Db_pid};
+                Employee_id =:= ""->
+                    io:format("update_login: Employee_id is empty."),
+                    {noreply,Db_pid};
+                true->
+                    db_api:store_data(Employer_id,num,Employee_id,
+                                        Db_pid),
                     {noreply,Db_pid}
             end
     end;
